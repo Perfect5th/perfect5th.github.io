817fc49e88d5453bb96e488ddfa2b17fa3192240
perfect5th <mitch.b45@gmail.com>
2016-11-19 12:47:09 -0800

    Animated posts, baby!

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
-$(document).ready(() => {
-  $('.post-list > li > h2').click(() => {
+$(document).ready(function() {
+  $('.post-item').animate({
+    opacity: 1,
+    top: 0,
+  }, 'slow');
+
+  /* $('.post-list > li > h2').click(() => {
     // $('.post-list > li').width(200);
-  });
+  }); */
 });

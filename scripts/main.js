2318d2c5c6dfadf0e3408d3d7a137e4081e1087d
perfect5th <mitch.b45@gmail.com>
2016-11-20 10:55:02 -0800

    Possible fixes for div overlap, baby!

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,12 @@ $(document).ready(function() {
   $('.post-item').animate({
     opacity: 1,
     top: 0,
-  }, 'slow');
+  }, 'slow', 'swing', function () {
+    $(this).css({
+      'z-index': 0,
+    });
+    $(this).parent().css('overflow', 'hidden');
+  });
 
   /* $('.post-list > li > h2').click(() => {
     // $('.post-list > li').width(200);

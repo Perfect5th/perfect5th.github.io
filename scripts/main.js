7d996dee35dd5034d8d199a02e26917215fbeab4
perfect5th <mitch.b45@gmail.com>
2016-11-25 18:49:09 -0800

    Have sliding open posts now, but they don't close yet, baby!

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   $('.post-box').animate({
     opacity: 1,
     top: 0,
@@ -9,10 +10,24 @@ $(document).ready(function() {
     $(this).parent().css('overflow', 'hidden');
   });
 
-  $('.post-box').click(function () {
+  $('.closed-post-box').click(function () {
+    var $curr_post = $(this);
     $(this).animate({
-      width: '97%',
+      width: '95%',
+    }, function () {
+      $curr_post.find('.close-post-box').show();
+      $.get($curr_post.children('.post-url').attr('id'), function (data) {
+        $curr_post.append('<div class="post-content-inline" hidden>' + data + '</div>');
+        $('.post-content-inline').slideDown(function () {
+          $curr_post.off()
+        });
+      });
     });
-    $.get()
   });
+
+  $('.close-post-box').click(function () {
+    var $curr_post = $(this);
+    alert('CLOSE THAT BOX');
+  });
+
 });

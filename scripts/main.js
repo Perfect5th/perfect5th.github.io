3325d0c296c494e6cca303d21909aff66627836f
Mitchell Burton <mitch.b45@gmail.com>
2017-11-09 21:54:26 -0800

    done the blog index, yay!

--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,21 @@
-$(function() {
+document.addEventListener('DOMContentLoaded', initElements);
 
-  // blog js
+function initElements() {
+  var p = document.getElementsByClassName('blog-list-item');
 
-  $('.post-box').animate({
-    opacity: 1,
-    top: 0,
-  }, 'slow', 'swing', function () {
-    $(this).css({
-      'z-index': 0,
-    });
-    $(this).parent().css('overflow', 'hidden');
-  });
-
-  function openPost() {
-    var $curr_post = $(this);
-    $curr_post.animate({ width: '92%' }, function () {
-      $curr_post.find('.close-post-box').show();
-      $curr_post.find('.post-content-inline').load($curr_post.children('.post-url').attr('id') + ' .post-content', function () {
-        $('.post-content-inline').slideDown(function () {
-          $curr_post.off();
-        });
-      });
-    });
+  for (var i = 0; i < p.length; i++) {
+    var item = p.item(i);
+    item.onmouseover = colorHeader;
+    item.onmouseout = revertHeader;
   }
+}
 
-  $('.closed-post-box').on('click', openPost);
+function colorHeader() {
+  var h2 = this.getElementsByTagName('h2').item(0);
+  h2.classList.add('hovered');
+}
 
-  $('.close-post-box').click(function () {
-    var $curr_post = $(this).parent();
-    var init_width = $curr_post.find('.post-item-title').width();
-    $(this).hide();
-    $curr_post.children('.post-content-inline').slideUp(function () {
-      $(this).remove();
-      $curr_post.animate({
-        width: init_width,
-      }, function () {
-        $curr_post.on('click', openPost);
-      });
-    });
-  });
-
-
-  // index js
-
-  function openHighlight() {
-      var $current = $(this);
-      $current.animate({
-          width: $current.parent().width() * 0.9,
-      }, function() {
-          $current.children('.highlight-content').slideDown();
-      });
-      $current.addClass('opened-highlight');
-      $current.removeClass('closed-highlight');
-      $current.off();
-      $current.on('click', closeHighlight);
-  }
-
-  function closeHighlight() {
-      var $current = $(this);
-      var init_width = "10em";
-      $current.children('.highlight-content').slideUp(400, function() {
-          $current.animate({
-              width: init_width
-          });
-      });
-      $current.removeClass('opened-highlight');
-      $current.addClass('closed-highlight');
-      $current.off();
-      $current.on('click', openHighlight);
-  }
-
-  $('.closed-highlight').on('click', openHighlight);
-
-});
+function revertHeader() {
+  var h2 = this.getElementsByTagName('h2').item(0);
+  h2.classList.remove('hovered');
+}

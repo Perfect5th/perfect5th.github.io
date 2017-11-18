bff53964ecdb7128f7feeb9215f21076e0d3c154
Mitchell Burton <mitch.b45@gmail.com>
2017-11-18 11:01:11 -0800

    working on colour-changing stuffs

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,14 @@ function initElements() {
     item.onmouseover = colorHeader;
     item.onmouseout = revertHeader;
   }
+
+  var pickers = document.getElementsByClassName('colour-choice');
+
+  for (var i = 0; i < pickers.length; i++) {
+    var item = pickers.item(i);
+
+    item.onclick = changeThemeColour;
+  }
 }
 
 function colorHeader() {
@@ -18,4 +26,23 @@ function colorHeader() {
 function revertHeader() {
   var h2 = this.getElementsByTagName('h2').item(0);
   h2.classList.remove('hovered');
+}
+
+function changeThemeColour() {
+  var themeColour = this.dataset.colour;
+  var root = document.getElementById('root');
+
+  if (root.classList.length > 0)
+    root.classList.replace(root.classList.item(0), themeColour);
+  else
+    root.classList.add(themeColour);
+
+  var otherColours = document.getElementsByClassName('colour-choice');
+  for (var i = 0; i < otherColours.length; i++) {
+    var item = otherColours.item(i);
+    if (item.classList.contains('active'))
+      item.classList.remove('active');
+  }
+
+  this.classList.add('active');
 }

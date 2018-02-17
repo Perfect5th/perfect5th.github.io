a09a0db2f9c4d98df6868ce266c0be7487cb96be
Mitchell Burton <mitch.b45@gmail.com>
2018-02-17 15:32:19 -0400

    finish nav menu on homepage

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,15 @@ function showNav(event) {
   navItems.classList.add('nav-items-shown');
   navItems.style.left = '0px';
 }
+
+function closeNav(event) {
+  var navItems = document.getElementById('nav-items');
+  var hamburger = document.getElementById('hamburger');
+
+  navItems.classList.remove('nav-items-shown');
+  navItems.style.left = '105px';
+
+  setTimeout(function () {
+    hamburger.style.display = 'block';
+  }, 500);
+}
